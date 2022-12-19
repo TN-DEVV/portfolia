8b5da611ff828cf094faf45887bd5a8e7f050cc8
marzaba007 <93664733+marzaba007@users.noreply.github.com>
2022-12-19 08:04:44 +0100

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,153 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+
+  {
+    numb: 6,
+    question: "The two levels of design for software architecture are:",
+    answer: "Data design and architectural design",
+    options: [
+      "Data design and architectural design",
+      "Pattern design and data design",
+      "Coding design and architectural design",
+      "Architectural design and XP design"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In software engineering, what does KDD stands for?",
+    answer: "Knowledge discovery in databases",
+    options: [
+      "Knowledge distortion of design",
+      "Knowledge discovery in databases",
+      "Knowledge development in the design",
+      "Knowledge detection of databases"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In software engineering, what does ATAM stands for?",
+    answer: "Architecture trade-off analysis method",
+    options: [
+      "Architecture type analysis management",
+      "Architecture type analysis method",
+      "Architecture trade-off analysis method",
+      "Application texture architectural method"
+    ]
+  },
+  {
+    numb: 9,
+    question: "To check the complexity of architecture, what could a programmer use?",
+    answer: "Dependencies",
+    options: [
+      "Coupling",
+      "Beta testing",
+      "Architecture testing",
+      "Dependencies"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the individual called who uses the product after it has been fully developed and marketed?",
+    answer: "End-user",
+    options: [
+      "First-user",
+      "Tester",
+      "Designer",
+      "End-user"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Examples of traditional process models are",
+    answer: "All of these",
+    options: [
+      "Waterfall",
+      "Incremental",
+      "Spiral",
+      "All of these"
+    ]
+  },
+  {
+    numb: 12,
+    question: "System software programmers usually work in",
+    answer: "Low-level language",
+    options: [
+      "High-level language",
+      "Low-level language",
+      "Self-managed language",
+      "Modified language"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
